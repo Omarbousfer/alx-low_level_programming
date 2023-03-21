8c531d24ae5d099a5956428f9f0800b9dee133fd
Omarbousfer <omar.bousfer@gmail.com>
2023-03-21 10:29:03 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,27 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
-			_putchar(i * j);
+		{
+			int value = i * j;
+
+			if (value < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(value + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(value / 10 + '0');
+				_putchar(value % 10 + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
 	}
 }
