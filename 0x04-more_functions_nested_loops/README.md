d031aab2384c9bfd8aa8ad494a319181482d6453
Omarbousfer <omar.bousfer@gmail.com>
2023-03-23 05:18:24 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+1) program have a function that checks for uppercase character
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(void);
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
