e2266172785692d9a4bb692abc93f19213320fd3
Omarbousfer <omar.bousfer@gmail.com>
2023-04-11 16:48:35 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * **alloc_grid - returns a pointer to a 
+ * **alloc_grid - returns a pointer to a
  * 2 dimensional array of integers
  * @width: integer
  * @height: integer
