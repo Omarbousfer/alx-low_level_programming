84570d6c842498d4808c1734bc0c4480fddf14b6
Omarbousfer <omar.bousfer@gmail.com>
2023-04-13 15:49:27 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ int _strlen(char *s)
  * *string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n:
+ * @n: integer
  * Return: if fails, it should return NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
