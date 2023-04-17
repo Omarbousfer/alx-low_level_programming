4623ef7539a6602e219f9131101639738b26f37b
Omarbousfer <omar.bousfer@gmail.com>
2023-04-17 08:31:44 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a new type struct
+ * name: char
+ * age: integer
+ * owner: char
+ *
+ * Description: poppy
+ */
+typedef struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+} dog;
