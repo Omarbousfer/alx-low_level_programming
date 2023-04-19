299d2c463ef83baa62ebbd6e23a25e17496e56c9
Omarbousfer <omar.bousfer@gmail.com>
2023-04-19 10:58:41 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: string
+ * @f: pointer of the function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (namee && f)
+		f(name);
+}
