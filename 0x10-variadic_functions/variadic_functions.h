ad27eb0c04f6f9987a0b378b39e72a4d93e0aa50
Omarbousfer <omar.bousfer@gmail.com>
2023-04-20 16:36:59 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,16 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * struct token - struct
+ * @token: list of types of arguments
+ * @f: float
+ */
+typedef struct token
+{
+        char *token;
+        void (*f)(char *, va_list);
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
