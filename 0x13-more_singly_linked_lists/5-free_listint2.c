7efe806ee8345bb64da702edf5ede8cc31a09eff
Omarbousfer <omar.bousfer@gmail.com>
2023-05-01 12:50:16 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	listint_t *node, *tmp;
+
+	if (!head)
 		return;
-	while (*head)
+	while (node)
 	{
-		free(*head);
-		*head = (*head)->next;
+		tmp = node;
+		node = node->next;
+		free(tmp);
 	}
-	head = NULL;
+	*head = NULL;
 }
