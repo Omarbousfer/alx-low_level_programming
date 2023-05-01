01158d777485b25b9727719ddb81bf5fb6856373
Omarbousfer <omar.bousfer@gmail.com>
2023-05-01 12:14:13 -0700

    corrected script

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint - Free listint_t.
+ * free_listint - Frees a list of integers
  * @head: data type pointer the head/next node
  * Return: 0 elements all free
  */
 void free_listint(listint_t *head)
 {
-	while (head != NULL)
+	listint_t *node;
+
+	while (head)
 	{
-		free(head);
+		node = head;
 		head = head->next;
+		free(node);
 	}
 }
