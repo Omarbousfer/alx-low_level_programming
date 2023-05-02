3a740b61968b517edcbf51160ce891be59ef91b9
Omarbousfer <omar.bousfer@gmail.com>
2023-05-02 12:03:13 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Free listint_t.
+ * free_listint2 - Free listint_t
  * @head: data type pointer the head/next node
  * Return: 0 elements all free
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *node, *tmp;
+	listint_t *node, *temp;
 
 	if (!head)
 		return;
 	while (node)
 	{
-		tmp = node;
+		temp = node;
 		node = node->next;
-		free(tmp);
+		free(temp);
 	}
 	*head = NULL;
 }
