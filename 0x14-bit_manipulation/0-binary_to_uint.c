518a130df760cfcf9c68508635d51f2225c4c46c
Omarbousfer <omar.bousfer@gmail.com>
2023-07-06 07:15:14 -0700

    fixed betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,8 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: type const char pointer of one bit
- * Return: converted number, or 0 if one or more char in str b that is not 0 or 1
+ * Return: converted number, or 0 if one or more char
+ * in str b that is not 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
