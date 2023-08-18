a75db54ea834910c8386e221721e2a037603e75b
Omarbousfer <omar.bousfer@gmail.com>
2023-08-18 11:03:26 -0700

    delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,51 +1,37 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a
- * dlistint_t linked list
- * @head: head of the list
- * @index: index of the new node
- * Return: 1 if it succeeded / -1 if it failed
+ * delete_dnodeint_at_index - delete node at index
+ * @head: type dpointer of next and prev node
+ * @index: type unisgned int index of node
+ * Return: 1 if success or -1 if node NULL
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *hd1;
-	dlistint_t *hd2;
-	unsigned int i;
+	dlistint_t *temp_node = *head;
 
-	hd1 = *head;
+	if (*head == NULL)
+		return (-1);
 
-	if (hd1 != NULL)
-		while (hd1->prev != NULL)
-			hd1 = hd1->prev;
-
-	i = 0;
-
-	while (hd1 != NULL)
+	for (; index != 0; index--)
 	{
-		if (i == index)
-		{
-			if (i == 0)
-			{
-				*head = hd1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				hd2->next = hd1->next;
-
-				if (hd1->next != NULL)
-					hd1->next->prev = h2;
-			}
-
-			free(hd1);
-			return (1);
-		}
-		hd2 = hd1;
-		hd1 = hd1->next;
-		i++;
+		if (temp_node == NULL)
+			return (-1);
+		temp_node = temp_node->next;
 	}
-
-	return (-1);
+	if (temp_node == *head)
+	{
+		*head = temp_node->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		temp_node->prev->next = temp_node->next;
+		if (temp_node->next != NULL)
+			temp_node->next->prev = temp_node->prev;
+	}
+	free(temp_node);
+	return (1);
 }
